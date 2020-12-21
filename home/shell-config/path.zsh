59f80f7197e0e411b4c836369900c2e13e2ce13e
Megan Tiu <megantiu@me.com>
2020-12-21 13:21:59 -0500

    Auto-run nvm use when nvmrc is present

--- home/shell-config/path.zsh
+++ home/shell-config/path.zsh
@@ -16,3 +16,26 @@ if which pyenv > /dev/null; then eval "$(pyenv init -)"; fi
 
 # Add RVM to PATH for scripting. Make sure this is the last PATH variable change.
 export PATH="$PATH:$HOME/.rvm/bin"
+
+# Call `nvm use` automatically in a directory with a .nvmrc file
+# https://github.com/nvm-sh/nvm#zsh
+autoload -U add-zsh-hook
+load-nvmrc() {
+  local node_version="$(nvm version)"
+  local nvmrc_path="$(nvm_find_nvmrc)"
+
+  if [ -n "$nvmrc_path" ]; then
+    local nvmrc_node_version=$(nvm version "$(cat "${nvmrc_path}")")
+
+    if [ "$nvmrc_node_version" = "N/A" ]; then
+      nvm install
+    elif [ "$nvmrc_node_version" != "$node_version" ]; then
+      nvm use
+    fi
+  elif [ "$node_version" != "$(nvm version default)" ]; then
+    echo "Reverting to nvm default version"
+    nvm use default
+  fi
+}
+add-zsh-hook chpwd load-nvmrc
+load-nvmrc
